be13cb55f85ed58a1e99900920c5ccd2fca9a584
solucion40 <ricardolau6@gmail.com>
2016-10-11 23:20:02 +0200

    Control de conexion a Base de Datos La propiedad connect_errno en mysqli solo muestra el error de la ultima conexión, pero como utilizamos varias conexiones, el problema surge que no sabermos si una ella falla. Por este motivo creamos , propiedad "controlError"

--- modulos/mod_conexion/conexionBaseDatos.php
+++ modulos/mod_conexion/conexionBaseDatos.php
@@ -1,20 +1,27 @@
 <?php 
-/* Realizamos conexion a la base de datos de importarRecambios. */
-$BDImportRecambios =new mysqli("localhost", "coches", "coches", "importarRecambios");
+/************************************************************************************************/
+/*************   Realizamos conexion de base de datos de ImportarRecambios.          ************/
+/************************************************************************************************/
+$BDImportRecambios = new mysqli("localhost", "coches", "coches", "iportarrecambios");
+// Como connect_errno , solo muestra el error de la ultima instrucción mysqli, tenemos que crear una propiedad, en la que 
+// está vacía, si no se produce error.
 if ($BDImportRecambios->connect_errno) {
-    echo "Falló la conexión a MySQL: (" . $BDImportRecambios->connect_errno . ") " . $BDImportRecambios->connect_error;
-}
+		$BDImportRecambios->controlError = $BDImportRecambios->connect_errno.':'.$BDImportRecambios->connect_error;
+} else {
 /** cambio del juego de caracteres a utf8 */
  mysqli_query ($BDImportRecambios,"SET NAMES 'utf8'");
-
-// Conexion de base de datos de Recambios.
-$BDRecambios = new mysqli("localhost", "coches", "coches", "Recambios");
-
-mysqli_query ($BDRecambios,"SET NAMES 'utf8'");
+}
+/************************************************************************************************/
+/*****************   Realizamos conexion de base de datos de Recambios.          ****************/
+/************************************************************************************************/
+$BDRecambios = @new mysqli("localhost", "coches", "coches", "Recambios");
+// Como connect_errno , solo muestra el error de la ultima instrucción mysqli, tenemos que crear una propiedad, en la que 
+// está vacía, si no se produce error.
 
 if ($BDRecambios->connect_errno) {
-    echo "Falló la conexión a MySQL: (" . $BDRecambios->connect_errno . ") " . $BDRecambios->connect_error;
-}
+		$BDRecambios->controlError = $BDRecambios->connect_errno.':'.$BDRecambios->connect_error;
+} else {
 /** cambio del juego de caracteres a utf8 */
  mysqli_query ($BDRecambios,"SET NAMES 'utf8'");
+}
 ?>
