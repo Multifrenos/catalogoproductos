1dce7806dc08e72adab0fc2cd9f423c7741c67c9
solucion40 <ricardolau6@gmail.com>
2016-10-16 15:06:19 +0200

    Cambio de estructura de tabla referenciascversion de base datos importar

--- BD/BDImportarRecambios/referenciascversiones.sql
+++ BD/BDImportarRecambios/referenciascversiones.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u2
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 14, 2016 at 12:14 PM
--- Server version: 5.5.50-0+deb8u1
--- PHP Version: 5.6.24-0+deb8u1
+-- Generation Time: Oct 16, 2016 at 11:33 AM
+-- Server version: 5.5.50-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,26 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `importarrecambios`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `referenciascversiones`
---
-
-CREATE TABLE IF NOT EXISTS `referenciascversiones` (
-  `RefProveedor` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `MarcaDescrip` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `ModeloVersion` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `VersionAcabado` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `kw` int(2) NOT NULL,
-  `cv` int(3) NOT NULL,
-  `Cm3` int(4) NOT NULL,
-  `Ncilindros` int(2) NOT NULL,
-  `FechaInici` date NOT NULL,
-  `FechaFinal` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `TipoCombustible` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
