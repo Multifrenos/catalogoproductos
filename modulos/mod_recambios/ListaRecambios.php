30830cadae7ab9e6948abd3d26135f506ce4efff
antonio <antonio@debian>
2016-10-21 13:21:24 +0200

    Inicio creacion de paginacion

--- modulos/mod_recambios/ListaRecambios.php
+++ modulos/mod_recambios/ListaRecambios.php
@@ -8,17 +8,118 @@
         
 	include ("./../mod_familias/ObjetoFamilias.php");
 	include ("./ObjetoRecambio.php");
-        $Dfamilias = new Familias;
+	// Creamos objeto familia y leemos familias para mostrar..
+    $Dfamilias = new Familias;
 	$Familias= $Dfamilias->LeerFamilias($BDRecambios);
-	
-	// Consultamos cuantos registros hay en Recambios:
+	// Creamos objeto Recambio para realizar las consultas..
 	$Crecambios = new Recambio;
-	$consulta = "SELECT * FROM `recambios`";
-	$ResRecambios = $Crecambios->ConsultaRecambios($BDRecambios,$consulta);
-		$TotalRecambios = $ResRecambios->num_rows;
-		$TotalPaginas = $TotalRecambios / 40 ;
-		$TotalPaginas = round($TotalPaginas, 0, PHP_ROUND_HALF_UP);   // Redondeo al alza...
-        ?>
+	// Realizamos consulta para saber cuantos registros tiene y hacer paginación.
+	// Ahora creamos array paginas.
+    // Estructura:
+    // paginas{
+	//		actual:
+	//		inicio:
+	//		ultima:
+	//		
+	//		next->
+	//			[id]
+	//		previo->
+	//			[id]
+	// 			
+    $paginas = array();
+	
+	
+	$limite = 40 ; // Esto puede ser variable ...
+	$ContarRecambios = $Crecambios->ConsultaRecambios($BDRecambios,"0","0");
+	$TotalRecambios = $ContarRecambios->num_rows;
+	$TotalPaginas = $TotalRecambios / $limite ;
+	
+	$paginas['Ultima'] = round($TotalPaginas, 0, PHP_ROUND_HALF_UP);   // Redondeo al alza...
+	$paginas['inicio'] = 1;
+	// =========       Creamos paginado      ===================  //
+	if ($_GET[pagina]) {
+	$paginas['Actual'] = $_GET[pagina];
+	$LinkpgActual =  '<a class="PaginaActual"'.$paginas['Actual'].'>'.$paginas['Actual'].'</a>';
+	} else {
+	// Si NO paremetro es la primera.
+	$paginas['Actual'] = 1;
+	$LinkpgActual = '<a href="./ListaRecambios.php?pagina=1">1</a>';
+	}
+	switch ($paginas['Actual']) {
+    case 1:
+        $paginaInicio = $paginas['Actual'];
+        $LinkpgInicio = $LinkpgActual;
+        break;
+    case $TotalPaginas:
+        $paginas['Ultima'] = $paginas['Actual'];
+        $LinkpgFinal = $LinkpgActual;
+        break;
+    }
+    
+    if ($paginas['Actual'] < $paginas['Ultima']) {
+		$difPg= $paginas['Ultima']- $paginas['Actual'];
+		if ($difPg > 6 ){
+			$difPg = 5; // Su hay mas 5, solo muestra 6
+			 
+		}
+		for ($i = 1; $i <= $difPg; $i++) {
+			if ($paginas['Actual']+$i != $paginas['Ultima']) {
+				$paginas['next'][$i] = $paginas['Actual']+ $i  ;
+			} 
+		}
+	}
+	if ($paginas['Actual'] > $paginas['inicio']) {
+		$difPg= $paginas['Actual'] - $paginas['inicio'];
+		if ($difPg >6 ){
+			$difPg = 6; // Recuerda que restamos una entrada, por eso es 5 paginas solo las muestra..
+		
+		}
+		for ($i = 1; $i < $difPg; $i++) {
+			if ($difPg == 1) {
+				$difp = 2;
+			} else {
+				$difp = $difPg;
+
+			}
+			$paginas['previo'][$i] = $paginas['Actual']-($difp-$i);
+		}
+	}
+	$htmlPG =  '<ul class="pagination">';
+    $Linkpg = '<li><a href="./ListaRecambios.php?pagina=';
+	if ($paginas['Actual'] != $paginas['inicio']){
+		$htmlPG = $htmlPG.$Linkpg.$paginas['inicio'].'">'."Inicio".'</a></li>';
+		$htmlPG = $htmlPG.'<li class="disabled"><a>'.'<<...>>'.'</...></a></li>';
+	} else {
+		$htmlPG = $htmlPG.'<li class="active"><a>'.$paginas['inicio'].'</a></li>';
+	
+	}
+	foreach ($paginas['previo'] as $pagina	) {
+		$htmlPG = $htmlPG.$Linkpg.$pagina.'">'.$pagina.'</a></li>';
+		
+	
+	}
+	if ($paginas['previo']){
+	// Pagina actual... 
+	$htmlPG = 	$htmlPG = $htmlPG.'<li class="active"><a>'.$paginas['Actual'].'</a></li>';
+	}
+	
+	foreach ($paginas['next'] as $pagina	) {
+		$htmlPG = $htmlPG.$Linkpg.$pagina.'">'.$pagina.'</a></li>';
+	
+	}
+	if ($pagina + 1 == $paginas['Ultima']){
+		$htmlPG = $htmlPG.$Linkpg.$paginas['Ultima'].'">'.$paginas['Ultima'].'</a></li>';
+	} else {
+		$htmlPG = $htmlPG.'<li class="disabled"><a>'.'<<...>>'.'</...></a></li>';
+		$htmlPG = $htmlPG.$Linkpg.$paginas['Ultima'].'">'.$paginas['Ultima'].'</a></li>';
+
+	}
+	
+	
+	
+	
+	$htmlPG = $htmlPG. '</ul>';
+	?>
       
     </head>
 
@@ -37,6 +138,8 @@
 				//~ echo '</pre>';
 				?>
             </div>
+			
+			<!--=================  Sidebar -- Menu y filtro =============== -->
 			<div class=" col-md-2">
 				<h4> Opciones Recambios</h4>
 				<ul>
@@ -67,60 +170,60 @@
 				?>
 				</form> 
 			</div>
+			<!--==========  Contenido: Buscador, paginador y lista recambios ========== -->
+
             <div class="col-md-10">
 				<h4>Recambios encontrados</h4>
-                <?php 	echo 'Recambios encontrados:'.$TotalRecambios.'<br/>';
-                		echo 'Pagina: 1 - '.$TotalPaginas;
+                <?php 	
+						echo 'Recambios encontrados:'.$TotalRecambios.'<br/>';
+                		echo $htmlPG;
+						
 				?>
-		<form class="form-horizontal" role="form">
+						
+				<form class="form-horizontal" role="form">
                     <div class="form-group">
 					<label>Buscar</label>
 					<input class="control-label col-md-6" type="text" name="Buscar" value="">
                     </div>
-                    <div class="form-group">
-                        <legend>Listado de Recambios</legend>
-                    </div>
+                   
                  </form>
                  <!-- TABLA DE PRODUCTOS -->
-		 <div>
-		 <table class="table table-striped">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>DESCRIPCION</th>
-					<th>COSTE</th>
-					<th>MARGEN</th>
-					<th>PVP</th>
-					<th>IDFABR</th>
+				<div>
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>DESCRIPCION</th>
+								<th>COSTE</th>
+								<th>MARGEN</th>
+								<th>PVP</th>
+								<th>IDFABR</th>
 
-				</tr>
-			</thead>
+							</tr>
+						</thead>
 			
-			<?php 
-			// Ahora meto los datos de la consulta.
-			
-			$recambios  = $Crecambios->ObtenerRecambios($BDRecambios);
-			    //~ echo '<pre>';
-			    //~ var_dump($recambios);
-			    //~ echo '</pre>';
-			foreach ($recambios['items'] as $recambio){ 
-			?>
-			<tr>
-				<td><?php echo $recambio['id']; ?></td>
-				<td><?php echo $recambio['Descripcion']; ?></td>
-				<td><?php echo $recambio['coste']; ?></td>
-				<td><?php echo $recambio['margen']; ?></td>
-				<td><?php echo $recambio['pvp']; ?></td>
-			</tr>
-			<?php 
-			}
-			?>
-			
-		</table>
-		 </div>
-		 
-                 
-             </div>
+						<?php 
+						// Ahora meto los datos de la consulta.
+						$recambios  = $Crecambios->ConsultaRecambios($BDRecambios,$limite,"0");
+						$recambios  = $Crecambios->ObtenerRecambios($recambios);
+						foreach ($recambios['items'] as $recambio){ 
+						?>
+
+						<tr>
+							<td><?php echo $recambio['id']; ?></td>
+							<td><?php echo $recambio['Descripcion']; ?></td>
+							<td><?php echo $recambio['coste']; ?></td>
+							<td><?php echo $recambio['margen']; ?></td>
+							<td><?php echo $recambio['pvp']; ?></td>
+						</tr>
+
+						<?php 
+						}
+						?>
+						
+					</table>
+				</div>
+			</div>
         </div>
     </body>
 </html>
--- modulos/mod_recambios/ObjetoRecambio.php
+++ modulos/mod_recambios/ObjetoRecambio.php
@@ -6,51 +6,43 @@
 
 class Recambio
 {
-    
-    
-    function ObtenerRecambios($BDRecambios)
+	function ObtenerRecambios($ResRecambios)
     {
-	$recambios= array();
-        $consulta = "SELECT * FROM `recambios` LIMIT 40 ";
-	$ResRecambios = $this->ConsultaRecambios($BDRecambios,$consulta);
-        //~ $ResRecambios = $BDRecambios->query($consulta);
-        //~ if ($ResRecambios == true){
-		//~ $recambios['conexion'] = 'Correcto,consulta todas familias';
-		//~ } else {
-		//~ $recambios['conexion'] = 'Error '.mysqli_error($BDRecambios);	
-		//~ return $resultado;
-		//~ // No continuamos..
-	//~ }
-	//~ // Ahora tenemos que montar el array de resultado...
-	$recambios['NItems'] = $ResRecambios->num_rows;
-	$i = 0;
-	$recambio = array();
-	while ($recambio = $ResRecambios->fetch_assoc()) {
-		$recambios['items'][$i]['id']= $recambio['id'];
-		$recambios['items'][$i]['Descripcion']= $recambio['Descripcion'];
-		$recambios['items'][$i]['coste']= $recambio['coste'];
-		$recambios['items'][$i]['margen']= $recambio['margen'];
-		$recambios['items'][$i]['pvp']= $recambio['pvp'];
+		$recambios['NItems'] = $ResRecambios->num_rows;
+		$i = 0;
+		$recambio = array();
+		while ($recambio = $ResRecambios->fetch_assoc()) {
+			$recambios['items'][$i]['id']= $recambio['id'];
+			$recambios['items'][$i]['Descripcion']= $recambio['Descripcion'];
+			$recambios['items'][$i]['coste']= $recambio['coste'];
+			$recambios['items'][$i]['margen']= $recambio['margen'];
+			$recambios['items'][$i]['pvp']= $recambio['pvp'];
 
-		
-		$i = $i+1;
-	}
-	return $recambios;
+			
+			$i = $i+1;
+		}
+		return $recambios;
     }
     
-    function ConsultaRecambios($BDRecambios,$consulta)
+    
+    
+    function ConsultaRecambios($BDRecambios,$limite,$desde)
     {
-	$recambios = array();
-	$ResRecambios = $BDRecambios->query($consulta);
-	 if ($ResRecambios == true){
-		$recambios['conexion'] = 'Correcto,consulta todas familias';
-		} else {
-		$recambios['conexion'] = 'Error '.mysqli_error($BDRecambios);	
-		return $resultado;
-		// No continuamos..
-	}
-	//~ $recambios['NItems'] = $ResRecambios->num_rows;
-	return $ResRecambios ;
+		if ($limite > 0){
+			$rango = "LIMIT ".$limite." OFFSET ".$desde;
+			} else {
+			$rango ="";
+		}
+        $consulta = "SELECT * FROM `recambios`".$rango;
+		$ResRecambios = $BDRecambios->query($consulta);
+		 //~ if ($ResRecambios == true){
+			//~ $recambios['conexion'] = 'Correcto,consulta todas familias';
+			//~ } else {
+			//~ $ResRecambios['conexion'] = 'Error '.mysqli_error($BDRecambios);	
+			//~ return $resultado;
+			//~ // No continuamos..
+		//~ }
+		return $ResRecambios ;
     }
     
     
